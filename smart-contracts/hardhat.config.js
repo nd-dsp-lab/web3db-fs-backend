b4ed17df24303d362d431feda19887931e1628c4
Will Armsworthy <warmswor@nd.edu>
2025-10-06 16:10:05 -0400

    Added deploy.js and successfully deployed smart contract

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -1,10 +1,24 @@
+const dotenv = require("dotenv");
 require("@nomiclabs/hardhat-ethers");
+
+dotenv.config()
+
+console.log("Environment variables loaded:");
+console.log("INFURA_API_KEY:", process.env.INFURA_API_KEY ? "Present" : "Missing");
+console.log("PRIVATE_KEY:", process.env.PRIVATE_KEY ? "Present" : "Missing");
+console.log("ETHERSCAN_API_KEY:", process.env.ETHERSCAN_API_KEY ? "Present" : "Missing");
 
 module.exports = {
   solidity: "0.8.28",
   networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545"
-    }
-  }
+      // localhost:
+      hardhat: {},
+      sepolia: {
+        url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+        accounts: [process.env.PRIVATE_KEY ? `0x${process.env.PRIVATE_KEY}` : undefined].filter(Boolean)
+      },
+    },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  }, 
 };
